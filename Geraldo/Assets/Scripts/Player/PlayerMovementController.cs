c4578131fa869bf66bf355e47493e2714725d5a1
Andrew Johnson <andrewtaylorjohnson@gmail.com>
2020-02-01 16:02:44 -0700

    Player controller; second pass;

--- Geraldo/Assets/Scripts/Player/PlayerMovementController.cs
+++ Geraldo/Assets/Scripts/Player/PlayerMovementController.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(PlayerInputManager))]
 public class PlayerMovementController : MonoBehaviour
 {
+    [Header("COMPONENET REFERENCES")]
     public Transform _playerRoot;
     private PlayerInputManager _inputManager;
+    public PlayerGrappleFollow _grappleMeshFollower;
+    public GameObject _grappleSlot;
+    public GameObject _grappleTarget;
+    public GameObject _visualGrappleSlot;
+    public LineRenderer _armLine;
 
+    private float _hDir;
+    private float _lookDir;
     private Vector3 _velocity;
-    public enum _grappleStates { _idle, _shooting, _retracting, _attached};
-    public _grappleStates _currentGrappleState;
+    private enum _grappleStates { _idle, _shooting, _retracting, _attached};
+    private _grappleStates _currentGrappleState;
 
     //public movement fields
+    [Header("MOVEMENT FIELDS")]
     public float _acceleration = 0.1f;
     [Range(0f, 1f)]
     public float _horizontalDrag = 0.95f;
@@ -21,37 +30,41 @@ public class PlayerMovementController : MonoBehaviour
     public float _gravity = 10f;
 
     //grapple fields
+    [Header("GRAPPLE FIELDS")]
     private float _radius;
     private float _rotationDirection;
-    public float _angle;
+    private float _angle;
     private Vector3 _attachPoint;
-    public GameObject _grappleSlot;
-    public GameObject _grapple;
     private Vector3 _targetPosition;
     public float _grappleSpeed = 10f;
     public float _maxGrappleDistance = 5f;
-
-    //debugging fields
-    [Header("DEBUGGING FIELDS")]
-    public bool _movePlayer = true;
+    private float _currentRotationSpeed;
+    public float _minimumRotationSpeed = 3f;
 
     private void Start()
     {
         _inputManager = GetComponent<PlayerInputManager>();
         _velocity = new Vector3(0f, 10f, 0f);
         _currentGrappleState = _grappleStates._idle;
+        _armLine.enabled = false;
     }
 
     private void Update()
     {
         //poll input
-        float hDir = _inputManager.GetHorizontal();
+        _hDir = _inputManager.GetHorizontal();
         bool grapple = _inputManager.GetGrapple();
+
+        if(Mathf.Abs(_hDir) > 0f)
+        {
+            _lookDir = Mathf.Sign(_hDir);
+        }
 
         switch(_currentGrappleState)
         {
             case _grappleStates._idle:
-                AirMovement(hDir);
+                AirMovement();
+                LookRotationFree();
 
                 if (grapple)
                 {
@@ -63,7 +76,8 @@ public class PlayerMovementController : MonoBehaviour
                 }
                 break;
             case _grappleStates._shooting:
-                AirMovement(hDir);
+                AirMovement();
+                LookRotationFree();
                 ShootGrapple();
 
                 if(grapple)
@@ -72,23 +86,24 @@ public class PlayerMovementController : MonoBehaviour
                 }
                 break;
             case _grappleStates._retracting:
-                AirMovement(hDir);
+                AirMovement();
+                LookRotationFree();
                 RetractGrapple();
                 break;
             case _grappleStates._attached:
-                GrappleMovement(hDir);
+                GrappleMovement();
+                LookRotationGrapple();
 
                 if(grapple)
                 {
-                    float currentSpeed = _velocity.magnitude;
                     Vector3 dir = (transform.position - _attachPoint).normalized;
                     if(_rotationDirection > 0f)
                     {
-                        _velocity = new Vector3(-dir.y, dir.x, 0f).normalized * currentSpeed;
+                        _velocity = new Vector3(-dir.y, dir.x, 0f).normalized * _currentRotationSpeed;
                     }
                     else
                     {
-                        _velocity = new Vector3(dir.y, -dir.x, 0f).normalized * currentSpeed;
+                        _velocity = new Vector3(dir.y, -dir.x, 0f).normalized * _currentRotationSpeed;
                     }
                     _currentGrappleState = _grappleStates._retracting;
                 }
@@ -96,19 +111,17 @@ public class PlayerMovementController : MonoBehaviour
             default:
                 break;
         }
+
+        _armLine.SetPosition(0, _visualGrappleSlot.transform.position);
+        _armLine.SetPosition(1, _grappleTarget.transform.position);
     }
 
-    private void AirMovement(float hDir)
+    private void AirMovement()
     {
-        if(!_movePlayer)
-        {
-            return;
-        }
-
         //air control
-        if (Mathf.Abs(hDir) > 0f)
+        if (Mathf.Abs(_hDir) > 0f)
         {
-            _velocity.x += hDir * _acceleration;
+            _velocity.x += _hDir * _acceleration;
         }
         else
         {
@@ -124,9 +137,14 @@ public class PlayerMovementController : MonoBehaviour
         transform.position += _velocity * Time.deltaTime;
     }
 
-    private void GrappleMovement(float hDir)
+    private void GrappleMovement()
     {
-        _angle += (_velocity.magnitude / _radius) * _rotationDirection;
+        if(_currentRotationSpeed < _minimumRotationSpeed)
+        {
+            _currentRotationSpeed = Mathf.Lerp(_currentRotationSpeed, _minimumRotationSpeed, Time.deltaTime * 3f);
+        }
+
+        _angle += (_currentRotationSpeed / _radius) * _rotationDirection;
         if(_angle > 360f)
         {
             _angle -= 360f;
@@ -139,18 +157,19 @@ public class PlayerMovementController : MonoBehaviour
         float y = _radius * Mathf.Sin(_angle * Mathf.Deg2Rad);
         transform.position = _attachPoint + new Vector3(x, y, 0f);
 
-        _grapple.transform.position = new Vector3(_attachPoint.x, _attachPoint.y, 0f);
+        _grappleTarget.transform.position = new Vector3(_attachPoint.x, _attachPoint.y, 0f);
     }
 
     private void ShootGrapple()
     {
-        _grapple.transform.parent = _playerRoot;
+        _grappleMeshFollower.GrappleStart();
+        _grappleTarget.transform.parent = _playerRoot;
 
-        float distanceFromPlayer = Vector3.Distance(transform.position, _grapple.transform.position);
-        float distanceToTarget = Vector3.Distance(_grapple.transform.position, _targetPosition);
+        float distanceFromPlayer = Vector3.Distance(transform.position, _grappleTarget.transform.position);
+        float distanceToTarget = Vector3.Distance(_grappleTarget.transform.position, _targetPosition);
         if (distanceFromPlayer < _maxGrappleDistance && distanceToTarget > 0.1f)
         {
-            _grapple.transform.position = Vector3.MoveTowards(_grapple.transform.position, _targetPosition, _grappleSpeed * Time.deltaTime);
+            _grappleTarget.transform.position = Vector3.MoveTowards(_grappleTarget.transform.position, _targetPosition, _grappleSpeed * Time.deltaTime);
         }
         else
         {
@@ -160,11 +179,12 @@ public class PlayerMovementController : MonoBehaviour
 
     private void RetractGrapple()
     {
-        _grapple.transform.position = Vector3.MoveTowards(_grapple.transform.position, _grappleSlot.transform.position, _grappleSpeed * Time.deltaTime);
-        if(Vector3.Distance(_grapple.transform.position, _grappleSlot.transform.position) < 0.1f)
+        _grappleMeshFollower.GrappleEnd();
+        _grappleTarget.transform.position = Vector3.MoveTowards(_grappleTarget.transform.position, _grappleSlot.transform.position, _grappleSpeed * Time.deltaTime);
+        if(Vector3.Distance(_grappleTarget.transform.position, _grappleSlot.transform.position) < 0.1f)
         {
-            _grapple.transform.parent = _grappleSlot.transform;
-            _grapple.transform.position = _grappleSlot.transform.position;
+            _grappleTarget.transform.parent = _grappleSlot.transform;
+            _grappleTarget.transform.position = _grappleSlot.transform.position;
             _currentGrappleState = _grappleStates._idle;
         }
     }
@@ -174,14 +194,15 @@ public class PlayerMovementController : MonoBehaviour
         _attachPoint = attachPoint;
         if (_currentGrappleState == _grappleStates._shooting)
         {
-            _grapple.transform.position = _attachPoint;
+            _grappleTarget.transform.position = _attachPoint;
             Vector3 dir = (transform.position - _attachPoint).normalized;
             _radius = Vector3.Distance(transform.position, _attachPoint);
             _angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
+            _currentRotationSpeed = _velocity.magnitude;
+
             //get angle from attach point
             float incomingAngle = Vector3.SignedAngle((attachPoint - transform.position).normalized, _velocity.normalized, Vector3.forward);
-            Debug.Log(incomingAngle);
 
             if(Mathf.Sign(incomingAngle) > 0)
             {
@@ -191,7 +212,41 @@ public class PlayerMovementController : MonoBehaviour
             {
                 _rotationDirection = 1;
             }
+
+            if(transform.position.x > _attachPoint.x)
+            {
+                _lookDir = -1;
+            }
+            else
+            {
+                _lookDir = 1;
+            }
             _currentGrappleState = _grappleStates._attached;
         }
+    }
+
+    private void LookRotationFree()
+    {
+        //rotate to movement direction
+        Quaternion targetRotation = Quaternion.Euler(0f, 0f, 0f);
+        if (_lookDir < 0f)
+        {
+            targetRotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5f * Time.deltaTime);
+    }
+
+    private void LookRotationGrapple()
+    {
+        Quaternion targetRotation;
+        if(_lookDir > 0f)
+        {
+            targetRotation = Quaternion.Euler(0f, 0f, _angle + 180f);
+        }
+        else
+        {
+            targetRotation = Quaternion.Euler(0f, 180f, -_angle);
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
     }
 }
