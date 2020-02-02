5da877cc7300873a6123d2296670c5e57aaf067e
Alex Johnstone <alexander.h.johnstone@gmail.com>
2020-02-02 11:59:58 -0700

    Lightbulb Emission Setup

--- Geraldo/Assets/Scripts/LightBulbController.cs
+++ Geraldo/Assets/Scripts/LightBulbController.cs
@@ -6,12 +6,15 @@ public class LightBulbController : MonoBehaviour
     [Range(0, 1)]
     private float intensityScale;
 
+    public LightbulbEmission _emissionController;
+
     public void SetIntensityScale(float rotationPercent, int playerDirection)
     {
         if (playerDirection == -1)
             intensityScale = 1 - Mathf.InverseLerp(0.5f, 1f, rotationPercent);
         else
             intensityScale = 1 - Mathf.InverseLerp(0.5f, 0f , rotationPercent);
-        //TODO do something with new intensity scale
+
+        if (_emissionController != null) _emissionController.SetLightLevel(intensityScale);
     }
 }
