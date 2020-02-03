2cc91d2ef29f3871de9a7c2ee935a0cd065de529
Alex Johnstone <alexander.h.johnstone@gmail.com>
2020-02-02 18:36:02 -0700

    Bulb & Killzone Tweaks, Level Normals

---
+++ Geraldo/Assets/Scripts/Utility/Screenshotter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshotter : MonoBehaviour
+{
+    public int _superScale = 1;
+    public string _name = "Screenshot_";
+
+    private int _number = 0;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ScreenCapture.CaptureScreenshot(_name + _number.ToString() + ".png", _superScale);
+            _number++;
+        }
+    }
+}
