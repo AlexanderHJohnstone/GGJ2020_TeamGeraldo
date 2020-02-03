f2f236639eb1b08273ae6ee01fa6437d41105fbb
Alex Johnstone <alexander.h.johnstone@gmail.com>
2020-02-02 17:02:41 -0700

    Background & Lighting

---
+++ Geraldo/Assets/Scripts/Player/PlayerLightFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLightFollow : MonoBehaviour
+{
+    public Transform _follow;
+
+    public float _speed = 0.1f;
+
+    private Vector3 _offset;
+
+
+
+    private void Start()
+    {
+        _offset = transform.position - _follow.position;
+    }
+
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, _follow.position + _offset, _speed);
+    }
+}
